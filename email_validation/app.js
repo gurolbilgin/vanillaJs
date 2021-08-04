b542c99cdb6143a45e4790aee19f6eb2a8717346
Gurol Bilgin <gurolbilgin@gmail.com>
2021-08-03 23:49:15 -0600

    adds minor changes

--- email_validation/app.js
+++ email_validation/app.js
@@ -16,7 +16,7 @@ emailAddress.addEventListener("keyup", (e) => {
 function validateEmail() {
     const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
    
-    const isValid = expression.test(String(emailAddress.value).toLowerCase())// test() takes steing as a parametre and tests whether the string has the value format indicated. Our current indicator is expression variable
+    const isValid = expression.test(String(emailAddress.value).toLowerCase())// test() takes steing as a parametre and tests whether the string has the value format indicated and it returns a boolean value true or false. Our current indicator is expression variable
 
 
         if (isValid) {
