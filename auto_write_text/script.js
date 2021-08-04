390822825fde49f5bfe94692885f24fdd1a4a1f6
Gurol Bilgin <gurolbilgin@gmail.com>
2021-08-04 06:46:48 -0600

    adds auto write text project

---
+++ auto_write_text/script.js
@@ -0,0 +1,18 @@
+const text = "codersclan is the best !!!!!!!"
+
+let index = 0;
+ 
+
+function writeText() {
+
+    document.body.innerText = text.slice(0, index);
+
+    index++;
+
+    if (index > text.length) {
+        index = 0;
+
+    }
+}
+
+setInterval(writeText, 100);
